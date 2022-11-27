e6c03b2db40724e53eac3736edde978a14ca162e
Vinit21592 <vinit.londhe21@gmail.com>
2022-11-27 19:47:22 +0530

    New modified files and folders

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_requirements()->List[str]:
 
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
         requirement_list = requirement_file.readlines()
-    requirement_list = [requirement_name.replace("\n","") for requirement_name in requirement_list]
+    requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
     
     if HYPHEN_E_DOT in requirement_list:
         requirement_list.remove(HYPHEN_E_DOT)
