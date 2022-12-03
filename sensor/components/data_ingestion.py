d84dd5f42b2093ebb8637d0eeb710d74f0bbd614
Vinit21592 <vinit.londhe21@gmail.com>
2022-12-03 23:18:24 +0530

    Updated code

--- sensor/components/data_ingestion.py
+++ sensor/components/data_ingestion.py
@@ -10,8 +10,9 @@ from sklearn.model_selection import train_test_split
 
 class DataIngestion:
     
-    def __init__(self,data_ingestion_config:config_entity.DataIngestionConfig ):
+    def __init__(self,data_ingestion_config:config_entity.DataIngestionConfig):
         try:
+            logging.info(f"{'>>'*20} Data Ingestion {'<<'*20}")
             self.data_ingestion_config = data_ingestion_config
         except Exception as e:
             raise SensorException(e, sys)
